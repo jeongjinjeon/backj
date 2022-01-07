a0796f6de1e2d405bc24540e5babf251a841c932
전정진 SKC&C/DevOps <jjjsins@partner.sk.com>
2022-01-07 17:40:58 +0900

    최초 커밋

---
+++ src/test/java/com/jjjsins/jjj/JjjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjjsins.jjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
