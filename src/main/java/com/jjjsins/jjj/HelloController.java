629a082d55c6141133253e68fe7bdd8819cfbef7
전정진 SKC&C/DevOps <jjjsins@partner.sk.com>
2022-01-10 14:35:02 +0900

    test

--- src/main/java/com/jjjsins/jjj/HelloController.java
+++ src/main/java/com/jjjsins/jjj/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 @GetMapping("/api/hello")
     public String hello(){
-    return "test \n";
+    return "test\n";
     }
 }
 
